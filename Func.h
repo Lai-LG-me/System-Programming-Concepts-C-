8565d008368504efea4f573c3e5e7071fea622e5
Leadgin <lgginnie0510@gmail.com>
2025-09-01 00:19:50 +0800

    Update Func.h

--- Func.h
+++ Func.h
@@ -5,15 +5,16 @@
 void booking();
 vector<VenueDetails> getFacility(string& filename);
 void Availability(Booking custBook);
+
 // Payment
+int daySelection();
+void paymentMethod(double totalPrice, Booking custBook, int daySelected);
+void cashPayment(double totalPrice, Booking custBook, int daySelected);
+void bankInPayment(double totalPrice, Booking custBook, int daySelected);
+void Payment_BookingDetailPage(Booking custBook, double totalPrice, int daySelected);
 void PaymentPage(Booking custBook, double totalPrice, int daySelected);
-void paymentMethod(double totalPrice);
-void cashPayment(double totalPrice);
-void bankInPayment(double totalPrice);
 void Payment(Booking custBook);
-
-void printReceipt();
-
+void printReceipt(const Booking& custBook, double totalPrice, int daySelected, int paymentChoice, double userPaid, double change);
 
 
 void printVenue(vector<VenueDetails>* venues);

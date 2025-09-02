9e3f578c4fdbbeaf5203f6e832b4867aedf85840
Leadgin <lgginnie0510@gmail.com>
2025-09-02 22:35:50 +0800

    Update Func.h

--- Func.h
+++ Func.h
@@ -11,10 +11,12 @@ int daySelection();
 void paymentMethod(double totalPrice, Booking custBook, int daySelected);
 void cashPayment(double totalPrice, Booking custBook, int daySelected);
 void bankInPayment(double totalPrice, Booking custBook, int daySelected);
-void Payment_BookingDetailPage(Booking custBook, double totalPrice, int daySelected);
-void PaymentPage(Booking custBook, double totalPrice, int daySelected);
+int deposit(double hallPrice);
+void Payment_BookingDetailPage(Booking custBook, double hallPrice, int daySelected, double totalPrice = 0);
+void PaymentPage(Booking custBook, double hallPrice, int daySelected);
 void Payment(Booking custBook);
-void printReceipt(const Booking& custBook, double totalPrice, int daySelected, int paymentChoice, double userPaid, double change);
+void printReceipt(const Booking& custBook, double hallPrice, int daySelected, int paymentChoice, double userPaid = 0, double change = 0);
 
 
 void printVenue(vector<VenueDetails>* venues);
+
